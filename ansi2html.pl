100e04a965a7d47efad98460c9129f0986d8c539
Kent Fredric <kentfredric@gmail.com>
2010-12-24 20:30:51 +1300

    add a few files

---
+++ ansi2html.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+#
+use strict;
+use warnings;
+
+my $content = do {
+  open my $fh, '<', $ARGV[0] or die "Cant open $ARGV[0] , $!";
+  local $/ = undef;
+  scalar <$fh>;
+};
+
+my $colors = {
+    31 => 'color: #FF0000',
+    32 => 'color: #00FF00',
+    33 => 'color: #5555FF',
+    34 => 'color: #FFFF00',
+    35 => 'color: #FF00FF',
+    36 => 'color: #00FFFF',
+    0  => '',
+};
+$content =~ s/ /&nbsp;/g;
+$content =~ s{\n}{<br />}g;
+
+$content =~ s{\e\[(\d+)m(.*?)(?=\e\[\d+m)}{
+    '<span style="' . $colors->{$1} . '">' . $2 . '</span>';
+}gsex;
+
+$content =~ s/\e\[\d+m//g;
+
+print '<pre style="background-color: #051005; font: monospace">' . $content . '</pre>';
